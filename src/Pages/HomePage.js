156569847a7c8f145bd4f880c1b4c83236571841
Daniela Sofia F Assis <danielasofiassis@gmail.com>
2025-11-10 23:53:41 +0000

    base-snack-mobile

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const HomePage = () => {
+  
+  return (
+
+    <View> 
+    <Text> PUC Minas </Text>
+    </View>
+  )
+
+}
+
+export default HomePage
